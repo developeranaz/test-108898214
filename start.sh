cbcefe66f3e48eb5dd0e449d938c4481e2c9b242
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-02-17 23:08:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo $PORT >/PORT
-sed -i "s/listen 80/listen $(cat /PORT)/g" /etc/nginx/sites-enabled/default
+sed -i "s/80/$(cat /PORT)/g" /etc/nginx/sites-enabled/default
+#sed -i "s/listen \[::]:80//g" /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
