cc0447ad26628ee692fc364fcb0dbd15d373db85
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-03-23 06:06:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# echo $PORT >/PORT
+echo $PORT >/PORT
 # sed -i "s/80/$PORT/g" /etc/nginx/http.d/default.conf
 #sed -i "s/listen \[::]:80//g" /etc/nginx/sites-enabled/default
 # nginx -g 'daemon off;'
+jupyter notebook --ip=0.0.0.0 --port=$(cat /PORT) --NotebookApp.token='' --NotebookApp.password=''
