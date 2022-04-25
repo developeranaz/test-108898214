8229fff548cd7b6b4b5217243d4057a289b5e155
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-04-25 08:32:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 echo $PORT >/PORT
+cd $REPONAME
+go run ./go/go_qbittorrent.go --PORT=$(cat /PORT)
+
 # sed -i "s/80/$PORT/g" /etc/nginx/http.d/default.conf
 #sed -i "s/listen \[::]:80//g" /etc/nginx/sites-enabled/default
 # nginx -g 'daemon off;'
