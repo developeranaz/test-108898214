7047f3876efee867309722d21594c8b70efaa915
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-04-26 20:23:30 +0530

    added loop instead of somethin ; )

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 echo $PORT >/PORT
 cd $REPONAME
+while :
+    do
 export PATH=$PATH:/usr/local/go/bin; go run ./go/go_qbittorrent.go --PORT=$(cat /PORT) --awebui="false" --awebuipath="vuetorrent"
-
-# sed -i "s/80/$PORT/g" /etc/nginx/http.d/default.conf
-#sed -i "s/listen \[::]:80//g" /etc/nginx/sites-enabled/default
-# nginx -g 'daemon off;'
-jupyter notebook --ip=0.0.0.0 --port=$(cat /PORT) --NotebookApp.token='' --NotebookApp.password=''
+done
