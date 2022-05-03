ea3a7d0df222134fb909f2b898c7949304030d5f
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-05-03 09:16:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ echo $PORT >/PORT
 cd $REPONAME
 while :
     do
-export PATH=$PATH:/usr/local/go/bin; go run ./go/test2.go --PORT=$(cat /PORT) --awebui="false" --awebuipath="vuetorrent" 
+export PATH=$PATH:/usr/local/go/bin; go run ./go/go_flag.go --PORT=$(cat /PORT) --awebui="false" --awebuipath="vuetorrent" -
 
-export PATH=$PATH:/usr/local/go/bin; go run ./go/go_qbittorrent.go --PORT=$(cat /PORT) --awebui="false" --awebuipath="vuetorrent"
+#export PATH=$PATH:/usr/local/go/bin; go run ./go/go_qbittorrent.go --PORT=$(cat /PORT) --awebui="false" --awebuipath="vuetorrent"
 done
