543dbd105619922a50054d1a0c58a7b2ec4d2a04
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2023-02-17 08:14:45 +0530

    Delete scripts directory

--- scripts/app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#aria2
-if grep -Fxq "ria" /$REPONAME/scripts/message
-then
-    echo "found"
-else
-   echo 'notfound'
-fi
--- scripts/tbot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-curl -s "https://api.telegram.org/bot$BOTTOKEN/getUpdates"|tail -1|sed 's/,"text":"/\n/g;s/"}}]}//g' |tail -1 >/$REPONAME/config/message
-sleep 10
